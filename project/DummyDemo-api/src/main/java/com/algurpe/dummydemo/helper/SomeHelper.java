4d751d3a82960aa8a025060a8622800ffab41588
alber gurpe <algurpe@gmail.com>
2016-04-18 01:48:23 +0200

    Added what I understand for SomeHelper class

---
+++ project/DummyDemo-api/src/main/java/com/algurpe/dummydemo/helper/SomeHelper.java
@@ -0,0 +1,14 @@
+package com.algurpe.dummydemo.helper;
+
+
+public class SomeHelper{
+
+	public SomeHelper(){
+
+    }
+    
+    public int addition( int value1, int value2 ){
+    	
+    	return value1 + value2;
+    }
+}
